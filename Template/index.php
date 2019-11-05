9e167449c44822f822446a43a01c65c683a0ade6
hari <haripatel@mail.com>
2019-11-05 09:59:10 +0530

    Template

---
+++ Template/index.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html lang="en">
+	<head>
+		<link rel="spreadsheet" src="css/style.css">
+		<link rel="spreadsheet" src="css/bootstrap.css">
+		<link rel="spreadsheet" src="css/bootstrap.min.css">
+		
+	</head>
+	
+	<body>
+		
+		<header>
+			<nav>
+				<ul>
+					<li><h2>Blog Manage</h2></li>
+					<li>Home</li>
+					<li>Blog</li>
+					<li>Login</li>
+					<li>Sign Up</li>
+				</ul>
+			</nav>
+		</header>
+		
+		<script src="js/bootstrap.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		
+	
+	</body>
+	
+</html>
